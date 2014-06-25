54bc87db7877fe52163a6736250ccd1ba58d2b90
apavlune <apavlune@localhost>
2014-06-25 14:11:02 -0400

    updating sql, core navigation bug fix

--- install/sql/install_equipment.sql
+++ install/sql/install_equipment.sql
@@ -48,3 +48,216 @@ VALUES ('equipment_status',NOW(),'internal equipment plugin list, do not modify'
 
 INSERT INTO `%TABLE_PREFIX%list` (`name`, `created`,`notes`,`updated`)
 VALUES ('equipment',NOW(),'internal equipment plugin list, do not modify',NOW()); 
+
+INSERT INTO `%TABLE_PREFIX%form` (`type`, `deletable`,`title`, `notes`, `created`, `updated`)
+VALUES ('G',0,'Equipment','Equipment internal form',NOW(),NOW()); 
+
+DELIMITER $$
+CREATE PROCEDURE `%TABLE_PREFIX%CreateEquipmentFormFields`()
+BEGIN
+	SET @form_id = (SELECT id FROM `%TABLE_PREFIX%form` WHERE title='Equipment');
+	SET @status_list_id = (SELECT id FROM `%TABLE_PREFIX%list` WHERE `name`='equipment_status');
+	SET @equipment_list_id = (SELECT id FROM `%TABLE_PREFIX%list` WHERE `name`='equipment');
+
+	IF (@form_id IS NOT NULL) AND (@status_list_id IS NOT NULL) AND (@equipment_list_id IS NOT NULL) then
+		INSERT INTO `%TABLE_PREFIX%form_field`
+			(`form_id`,
+			`type`,
+			`label`,
+			`required`,
+			`private`,
+			`edit_mask`,
+			`name`,
+			`sort`,
+			`created`,
+			`updated`)
+			VALUES
+			(@form_id,
+			CONCAT('list-',@equipment_list_id),
+			'Equipment',
+			0,0,0,
+			'equipment',			
+			1,			
+			NOW(),
+			NOW());	
+
+		INSERT INTO `%TABLE_PREFIX%form_fieldform_field`
+			(`form_id`,
+			`type`,
+			`label`,
+			`required`,
+			`private`,
+			`edit_mask`,
+			`name`,
+			`sort`,
+			`created`,
+			`updated`)
+			VALUES
+			(@form_id,
+			CONCAT('list-',@status_list_id),
+			'Status',
+			0,0,0,
+			'status',			
+			2,			
+			NOW(),
+			NOW());					
+	END IF;
+END$$
+DELIMITER ;
+
+call `%TABLE_PREFIX%CreateEquipmentFormFields`;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS `%TABLE_PREFIX%equipment_ADEL`$$
+
+CREATE TRIGGER `%TABLE_PREFIX%equipment_ADEL` AFTER DELETE ON `%TABLE_PREFIX%equipment` FOR EACH ROW
+BEGIN
+	SET @pk=OLD.equipment_id;
+	SET @list_pk=(SELECT id FROM `%TABLE_PREFIX%list` WHERE name='equipment');
+	DELETE FROM `%TABLE_PREFIX%list_items` WHERE list_id = @list_pk AND properties=CONCAT('',@pk); 
+END$$
+DELIMITER ;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS `%TABLE_PREFIX%equipment_AINS`$$
+
+CREATE TRIGGER `%TABLE_PREFIX%equipment_AINS` AFTER INSERT ON `%TABLE_PREFIX%equipment` FOR EACH ROW
+BEGIN
+	SET @pk=NEW.equipment_id;
+	SET @list_pk=(SELECT id FROM `%TABLE_PREFIX%list` WHERE name='equipment');
+	INSERT INTO `%TABLE_PREFIX%list_items` (list_id, `value`, `properties`)
+	VALUES (@list_pk, NEW.name, CONCAT('',@pk)); 
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS `%TABLE_PREFIX%equipment_AUPD`$$
+
+CREATE TRIGGER `%TABLE_PREFIX%equipment_AUPD` AFTER UPDATE ON `%TABLE_PREFIX%equipment` FOR EACH ROW
+BEGIN
+	SET @pk=NEW.equipment_id;
+	SET @list_pk=(SELECT id FROM `%TABLE_PREFIX%list` WHERE name='equipment');
+
+	IF NEW.is_active = 0 THEN
+		DELETE FROM `%TABLE_PREFIX%list_items` WHERE list_id = @list_pk AND properties=CONCAT('',@pk);
+	ELSE
+		SET @list_item_pkid = (SELECT id 
+							   FROM `%TABLE_PREFIX%items `
+							   WHERE list_id = @list_pk AND properties=CONCAT('',@pk));
+
+		IF (@list_item_pkid IS NOT NULL) AND (@list_item_pkid>0) THEN
+			UPDATE `%TABLE_PREFIX%items` SET `value`= NEW.name 
+			WHERE `properties`= CONCAT('',@pk) AND list_id=@list_pk;
+		ELSE
+			INSERT INTO `%TABLE_PREFIX%items` (list_id, `value`, `properties`)
+			VALUES (@list_pk, NEW.name, CONCAT('',@pk));			
+		END IF;
+	END IF; 
+END$$
+DELIMITER ;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS `%TABLE_PREFIX%equipment_status_AINS`$$
+
+CREATE TRIGGER `%TABLE_PREFIX%equipment_status_AINS` AFTER INSERT ON `%TABLE_PREFIX%equipment_status` FOR EACH ROW
+BEGIN
+	SET @pk=NEW.status_id;
+	SET @list_pk=(SELECT id FROM `%TABLE_PREFIX%list` WHERE name='equipment_status');
+	INSERT INTO `%TABLE_PREFIX%list_items` (list_id, `value`, `properties`) 
+	VALUES (@list_pk, NEW.name, CONCAT('',@pk));
+END $$
+DELIMITER ;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS `%TABLE_PREFIX%equipment_status_AUPD`$$
+
+CREATE TRIGGER `%TABLE_PREFIX%equipment_status_AUPD` AFTER UPDATE ON `%TABLE_PREFIX%equipment_status` FOR EACH ROW
+BEGIN
+	SET @pk=NEW.status_id;
+	SET @list_pk=(SELECT id FROM `%TABLE_PREFIX%list` WHERE name='equipment_status'); 
+	UPDATE `%TABLE_PREFIX%list_items` SET `value`= NEW.name WHERE `properties`= CONCAT('',@pk) AND list_id=@list_pk;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS `%TABLE_PREFIX%equipment_status_ADEL`$$
+
+CREATE TRIGGER `%TABLE_PREFIX%equipment_status_ADEL` AFTER DELETE ON `%TABLE_PREFIX%equipment_status` FOR EACH ROW
+BEGIN
+	SET @pk=OLD.status_id; 
+	SET @list_pk=(SELECT id FROM `%TABLE_PREFIX%list` WHERE name='equipment_status');
+	DELETE FROM `%TABLE_PREFIX%list_items` WHERE list_id = @list_pk AND properties=CONCAT('',@pk);
+END $$
+DELIMITER ;
+
+
+CREATE VIEW `%TABLE_PREFIX%EquipmentFormView` AS 
+select `%TABLE_PREFIX%form`.`title` AS `title`,
+`%TABLE_PREFIX%form_entry`.`id` AS `entry_id`,
+`%TABLE_PREFIX%form_entry`.`object_id` AS `ticket_id`,
+`%TABLE_PREFIX%form_field`.`id` AS `field_id`,
+`%TABLE_PREFIX%form_field`.`label` AS `field_label`,
+`%TABLE_PREFIX%form_entry_values`.`value` AS `value`,
+`%TABLE_PREFIX%equipment`.`equipment_id` AS `equipment_id`,
+`%TABLE_PREFIX%equipment_status`.`status_id` AS `status_id` 
+from ((((`%TABLE_PREFIX%form_field` 
+left join 
+(`%TABLE_PREFIX%form_entry_values` join `%TABLE_PREFIX%form_entry`) 
+on(((`%TABLE_PREFIX%form_field`.`id` = `%TABLE_PREFIX%form_entry_values`.`field_id`) and 
+(`%TABLE_PREFIX%form_entry`.`id` = `%TABLE_PREFIX%form_entry_values`.`entry_id`)))) 
+left join `%TABLE_PREFIX%form` 
+on((`%TABLE_PREFIX%form`.`id` = `%TABLE_PREFIX%form_field`.`form_id`))) 
+left join `%TABLE_PREFIX%equipment` 
+on((`%TABLE_PREFIX%form_entry_values`.`value` = `%TABLE_PREFIX%equipment`.`name`))) 
+left join `%TABLE_PREFIX%equipment_status` 
+on((`%TABLE_PREFIX%form_entry_values`.`value` = `%TABLE_PREFIX%equipment_status`.`name`))) 
+where ((`%TABLE_PREFIX%form`.`title` = 'Equipment') and 
+(`%TABLE_PREFIX%form`.`id` = `%TABLE_PREFIX%form_entry`.`form_id`) and 
+(`%TABLE_PREFIX%form_entry`.`object_type` = 'T'));
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS `%TABLE_PREFIX%ticket_event_AINS`$$
+
+CREATE TRIGGER `%TABLE_PREFIX%ticket_event_AINS` AFTER INSERT ON `%TABLE_PREFIX%ticket_event` FOR EACH ROW
+BEGIN
+	IF NEW.state='closed' THEN
+		SET @equipment_id = (SELECT equipment_id FROM `%TABLE_PREFIX%equipment_ticket `
+							WHERE ticket_id=NEW.ticket_id LIMIT 1);
+		IF ((@equipment_id IS NOT NULL) AND (@equipment_id>0)) THEN
+			SET @status_id = (SELECT status_id FROM `%TABLE_PREFIX%equipment_status`
+							WHERE baseline=1 LIMIT 1);
+
+			IF ((@status_id IS NOT NULL) AND (@status_id>0)) THEN
+				UPDATE `%TABLE_PREFIX%equipment` SET status_id = @status_id
+				WHERE equipment_id = @equipment_id; 
+			END IF;
+		END IF;
+
+	ELSEIF NEW.state='created' THEN
+		
+		SET @status_id = (SELECT status_id FROM `%TABLE_PREFIX%EquipmentFormView` WHERE 
+						ticket_id= NEW.ticket_id AND field_label='Status' LIMIT 1);
+		SET @equipment_id = (SELECT equipment_id FROM `%TABLE_PREFIX%EquipmentFormView` WHERE 
+							ticket_id= NEW.ticket_id AND field_label='Equipment' LIMIT 1);
+
+		IF ((@status_id IS NOT NULL) AND 
+			(@status_id >0)) AND 
+			((@equipment_id IS NOT NULL) AND 
+			(@equipment_id >0)) THEN						
+				
+				UPDATE `%TABLE_PREFIX%equipment` SET status_id = @status_id WHERE equipment_id=@equipment_id;
+				INSERT INTO `%TABLE_PREFIX%equipment_ticket` (equipment_id, ticket_id, created) 
+				VALUES (@equipment_id, NEW.ticket_id, NOW());
+		END IF;
+	
+	END IF;
+END$$
+DELIMITER ;
---
+++ ost_core/include/class.nav.php
@@ -0,0 +1,348 @@
+<?php
+/*********************************************************************
+    class.nav.php
+
+    Navigation helper classes. Pointless BUT helps keep navigation clean and free from errors.
+
+    Peter Rotich <peter@osticket.com>
+    Copyright (c)  2006-2013 osTicket
+    http://www.osticket.com
+
+    Released under the GNU General Public License WITHOUT ANY WARRANTY.
+    See LICENSE.TXT for details.
+
+    vim: expandtab sw=4 ts=4 sts=4:
+**********************************************************************/
+require_once(INCLUDE_DIR.'class.app.php');
+
+class StaffNav {
+    var $tabs=array();
+    var $submenus=array();
+
+    var $activetab;
+    var $activemenu;
+    var $panel;
+
+    var $staff;
+
+    function StaffNav($staff, $panel='staff'){
+        $this->staff=$staff;
+        $this->panel=strtolower($panel);
+        $this->tabs=$this->getTabs();
+        $this->submenus=$this->getSubMenus();
+    }
+
+    function getPanel(){
+        return $this->panel;
+    }
+
+    function isAdminPanel(){
+        return (!strcasecmp($this->getPanel(),'admin'));
+    }
+
+    function isStaffPanel() {
+        return (!$this->isAdminPanel());
+    }
+
+    function getRegisteredApps() {
+        return Application::getStaffApps();
+    }
+
+    function setTabActive($tab, $menu=''){
+
+        if($this->tabs[$tab]){
+            $this->tabs[$tab]['active']=true;
+            if($this->activetab && $this->activetab!=$tab && $this->tabs[$this->activetab])
+                 $this->tabs[$this->activetab]['active']=false;
+
+            $this->activetab=$tab;
+            if($menu) $this->setActiveSubMenu($menu, $tab);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    function setActiveTab($tab, $menu=''){
+        return $this->setTabActive($tab, $menu);
+    }
+
+    function getActiveTab(){
+        return $this->activetab;
+    }
+
+    function setActiveSubMenu($mid, $tab='') {
+        if(is_numeric($mid))
+            $this->activeMenu = $mid;
+        elseif($mid && $tab && ($subNav=$this->getSubNav($tab))) {
+            foreach($subNav as $k => $menu) {
+                if(strcasecmp($mid, $menu['href'])) continue;
+
+                $this->activeMenu = $k+1;
+                break;
+            }
+        }
+    }
+
+    function getActiveMenu() {
+        return $this->activeMenu;
+    }
+
+    function addSubMenu($item,$active=false){
+
+        $this->submenus[$this->getPanel().'.'.$this->activetab][]=$item;
+        if($active)
+            $this->activeMenu=sizeof($this->submenus[$this->getPanel().'.'.$this->activetab]);
+    }
+
+
+    function getTabs(){
+
+        if(!$this->tabs) {
+            $this->tabs=array();
+            $this->tabs['dashboard'] = array('desc'=>'Dashboard','href'=>'dashboard.php','title'=>'Staff Dashboard');
+            $this->tabs['users'] = array('desc' => 'Users', 'href' => 'users.php', 'title' => 'User Directory');
+            $this->tabs['tickets'] = array('desc'=>'Tickets','href'=>'tickets.php','title'=>'Ticket Queue');
+            $this->tabs['kbase'] = array('desc'=>'Knowledgebase','href'=>'kb.php','title'=>'Knowledgebase');
+            if (count($this->getRegisteredApps()))
+                $this->tabs['apps']=array('desc'=>'Applications','href'=>'apps.php','title'=>'Applications');
+        }
+
+        return $this->tabs;
+    }
+
+    function getSubMenus(){ //Private.
+
+        $staff = $this->staff;
+        $submenus=array();
+        foreach($this->getTabs() as $k=>$tab){
+            $subnav=array();
+            switch(strtolower($k)){
+                case 'tickets':
+                    $subnav[]=array('desc'=>'Tickets','href'=>'tickets.php','iconclass'=>'Ticket', 'droponly'=>true);
+                    if($staff) {
+                        if(($assigned=$staff->getNumAssignedTickets()))
+                            $subnav[]=array('desc'=>"My&nbsp;Tickets ($assigned)",
+                                            'href'=>'tickets.php?status=assigned',
+                                            'iconclass'=>'assignedTickets',
+                                            'droponly'=>true);
+
+                        if($staff->canCreateTickets())
+                            $subnav[]=array('desc'=>'New&nbsp;Ticket',
+                                            'title' => 'Open New Ticket',
+                                            'href'=>'tickets.php?a=open',
+                                            'iconclass'=>'newTicket',
+                                            'id' => 'new-ticket',
+                                            'droponly'=>true);
+                    }
+                    break;
+                case 'dashboard':
+                    $subnav[]=array('desc'=>'Dashboard','href'=>'dashboard.php','iconclass'=>'logs');
+                    $subnav[]=array('desc'=>'Staff&nbsp;Directory','href'=>'directory.php','iconclass'=>'teams');
+                    $subnav[]=array('desc'=>'My&nbsp;Profile','href'=>'profile.php','iconclass'=>'users');
+                    break;
+                case 'users':
+                    $subnav[] = array('desc' => 'User&nbsp;Directory', 'href' => 'users.php', 'iconclass' => 'teams');
+                    $subnav[] = array('desc' => 'Organizations', 'href' => 'orgs.php', 'iconclass' => 'departments');
+                    break;
+                case 'kbase':
+                    $subnav[]=array('desc'=>'FAQs','href'=>'kb.php', 'urls'=>array('faq.php'), 'iconclass'=>'kb');
+                    if($staff) {
+                        if($staff->canManageFAQ())
+                            $subnav[]=array('desc'=>'Categories','href'=>'categories.php','iconclass'=>'faq-categories');
+                        if($staff->canManageCannedResponses())
+                            $subnav[]=array('desc'=>'Canned&nbsp;Responses','href'=>'canned.php','iconclass'=>'canned');
+                    }
+                   break;
+                case 'apps':
+                    foreach ($this->getRegisteredApps() as $app)
+                        $subnav[] = $app;
+                    break;
+            }
+            if($subnav)
+                $submenus[$this->getPanel().'.'.strtolower($k)]=$subnav;
+        }
+
+        return $submenus;
+    }
+
+    function getSubMenu($tab=null){
+        $tab=$tab?$tab:$this->activetab;
+        return $this->submenus[$this->getPanel().'.'.$tab];
+    }
+
+    function getSubNav($tab=null){
+        return $this->getSubMenu($tab);
+    }
+
+}
+
+class AdminNav extends StaffNav{
+
+    function AdminNav($staff){
+        parent::StaffNav($staff, 'admin');
+    }
+
+    function getRegisteredApps() {
+        return Application::getAdminApps();
+    }
+
+    function getTabs(){
+
+
+        if(!$this->tabs){
+
+            $tabs=array();
+            $tabs['dashboard']=array('desc'=>'Dashboard','href'=>'logs.php','title'=>'Admin Dashboard');
+            $tabs['settings']=array('desc'=>'Settings','href'=>'settings.php','title'=>'System Settings');
+            $tabs['manage']=array('desc'=>'Manage','href'=>'helptopics.php','title'=>'Manage Options');
+            $tabs['emails']=array('desc'=>'Emails','href'=>'emails.php','title'=>'Email Settings');
+            $tabs['staff']=array('desc'=>'Staff','href'=>'staff.php','title'=>'Manage Staff');
+            if (count($this->getRegisteredApps()))
+                $tabs['apps']=array('desc'=>'Applications','href'=>'apps.php','title'=>'Applications');
+            $this->tabs=$tabs;
+        }
+
+        return $this->tabs;
+    }
+
+    function getSubMenus(){
+
+        $submenus=array();
+        foreach($this->getTabs() as $k=>$tab){
+            $subnav=array();
+            switch(strtolower($k)){
+                case 'dashboard':
+                    $subnav[]=array('desc'=>'System&nbsp;Logs','href'=>'logs.php','iconclass'=>'logs');
+                    $subnav[]=array('desc'=>'Information','href'=>'system.php','iconclass'=>'preferences');
+                    break;
+                case 'settings':
+                    $subnav[]=array('desc'=>'Company','href'=>'settings.php?t=pages','iconclass'=>'pages');
+                    $subnav[]=array('desc'=>'System','href'=>'settings.php?t=system','iconclass'=>'preferences');
+                    $subnav[]=array('desc'=>'Tickets','href'=>'settings.php?t=tickets','iconclass'=>'ticket-settings');
+                    $subnav[]=array('desc'=>'Emails','href'=>'settings.php?t=emails','iconclass'=>'email-settings');
+                    $subnav[]=array('desc'=>'Access','href'=>'settings.php?t=access','iconclass'=>'users');
+                    $subnav[]=array('desc'=>'Knowledgebase','href'=>'settings.php?t=kb','iconclass'=>'kb-settings');
+                    $subnav[]=array('desc'=>'Autoresponder','href'=>'settings.php?t=autoresp','iconclass'=>'email-autoresponders');
+                    $subnav[]=array('desc'=>'Alerts&nbsp;&amp;&nbsp;Notices','href'=>'settings.php?t=alerts','iconclass'=>'alert-settings');
+                    break;
+                case 'manage':
+                    $subnav[]=array('desc'=>'Help&nbsp;Topics','href'=>'helptopics.php','iconclass'=>'helpTopics');
+                    $subnav[]=array('desc'=>'Ticket&nbsp;Filters','href'=>'filters.php',
+                                        'title'=>'Ticket&nbsp;Filters','iconclass'=>'ticketFilters');
+                    $subnav[]=array('desc'=>'SLA&nbsp;Plans','href'=>'slas.php','iconclass'=>'sla');
+                    $subnav[]=array('desc'=>'API&nbsp;Keys','href'=>'apikeys.php','iconclass'=>'api');
+                    $subnav[]=array('desc'=>'Pages', 'href'=>'pages.php','title'=>'Pages','iconclass'=>'pages');
+                    $subnav[]=array('desc'=>'Forms','href'=>'forms.php','iconclass'=>'forms');
+                    $subnav[]=array('desc'=>'Lists','href'=>'lists.php','iconclass'=>'lists');
+                    $subnav[]=array('desc'=>'Plugins','href'=>'plugins.php','iconclass'=>'api');
+                    break;
+                case 'emails':
+                    $subnav[]=array('desc'=>'Emails','href'=>'emails.php', 'title'=>'Email Addresses', 'iconclass'=>'emailSettings');
+                    $subnav[]=array('desc'=>'Banlist','href'=>'banlist.php',
+                                        'title'=>'Banned&nbsp;Emails','iconclass'=>'emailDiagnostic');
+                    $subnav[]=array('desc'=>'Templates','href'=>'templates.php','title'=>'Email Templates','iconclass'=>'emailTemplates');
+                    $subnav[]=array('desc'=>'Diagnostic','href'=>'emailtest.php', 'title'=>'Email Diagnostic', 'iconclass'=>'emailDiagnostic');
+                    break;
+                case 'staff':
+                    $subnav[]=array('desc'=>'Staff&nbsp;Members','href'=>'staff.php','iconclass'=>'users');
+                    $subnav[]=array('desc'=>'Teams','href'=>'teams.php','iconclass'=>'teams');
+                    $subnav[]=array('desc'=>'Groups','href'=>'groups.php','iconclass'=>'groups');
+                    $subnav[]=array('desc'=>'Departments','href'=>'departments.php','iconclass'=>'departments');
+                    break;
+                case 'apps':
+                    foreach ($this->getRegisteredApps() as $app)
+                        $subnav[] = $app;
+                    break;
+            }
+            if($subnav)
+                $submenus[$this->getPanel().'.'.strtolower($k)]=$subnav;
+        }
+
+        return $submenus;
+    }
+}
+
+class UserNav {
+
+    var $navs=array();
+    var $activenav;
+
+    var $user;
+
+    function UserNav($user=null, $active=''){
+
+        $this->user=$user;
+        $this->navs=$this->getNavs();
+        if($active)
+            $this->setActiveNav($active);
+    }
+
+    function getRegisteredApps() {
+        return Application::getClientApps();
+    }
+
+    function setActiveNav($nav){
+
+        if($nav && $this->navs[$nav]){
+            $this->navs[$nav]['active']=true;
+            if($this->activenav && $this->activenav!=$nav && $this->navs[$this->activenav])
+                 $this->navs[$this->activenav]['active']=false;
+
+            $this->activenav=$nav;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    function getNavLinks(){
+        global $cfg;
+
+        //Paths are based on the root dir.
+        if(!$this->navs){
+
+            $navs = array();
+            $user = $this->user;
+            $navs['home']=array('desc'=>'Support&nbsp;Center&nbsp;Home','href'=>'index.php','title'=>'');
+            if($cfg && $cfg->isKnowledgebaseEnabled())
+                $navs['kb']=array('desc'=>'Knowledgebase','href'=>'kb/index.php','title'=>'');
+
+            // Show the "Open New Ticket" link unless BOTH client
+            // registration is disabled and client login is required for new
+            // tickets. In such a case, creating a ticket would not be
+            // possible for web clients.
+            if ($cfg->getClientRegistrationMode() != 'disabled'
+                    || !$cfg->isClientLoginRequired())
+                $navs['new']=array('desc'=>'Open&nbsp;New&nbsp;Ticket','href'=>'open.php','title'=>'');
+            if($user && $user->isValid()) {
+                if(!$user->isGuest()) {
+                    $navs['tickets']=array('desc'=>sprintf('Tickets&nbsp;(%d)',$user->getNumTickets()),
+                                           'href'=>'tickets.php',
+                                            'title'=>'Show all tickets');
+                } else {
+                    $navs['tickets']=array('desc'=>'View&nbsp;Ticket&nbsp;Thread',
+                                           'href'=>sprintf('tickets.php?id=%d',$user->getTicketId()),
+                                           'title'=>'View ticket status');
+                }
+            } else {
+                $navs['status']=array('desc'=>'Check Ticket Status','href'=>'view.php','title'=>'');
+            }
+            
+            foreach ($this->getRegisteredApps() as $app)
+                        $navs[] = $app;                    
+            $this->navs=$navs;
+        }
+
+        return $this->navs;
+    }
+
+    function getNavs(){
+        return $this->getNavLinks();
+    }
+
+}
+
+?>
